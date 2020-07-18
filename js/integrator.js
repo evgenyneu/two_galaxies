d781dda12b806ed6490a8632c0e2a294b645d1ea
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-18 12:58:42 +1000

    Integrator

--- js/integrator.js
+++ js/integrator.js
@@ -1,6 +1,22 @@
+import * as vector from '../js/vector.js';
+import getAccelerations from '../js/acceleration.js';
+
 export default function integrateOneStep(timeStep, masses, positions,
-                                         velocities) {
-  var accelerations = 1;
+                                         velocities, accelerations) {
+  for(let i = 0; i < positions.length; i++) {
+    velocities[i] = vector.add(
+      velocities[i], vector.multiplyByNumber(0.5 * timeStep, accelerations[i]));
+
+    positions[i] = vector.add(
+      positions[i], vector.multiplyByNumber(timeStep, velocities[i]));
+  }
+
+  accelerations = getAccelerations(masses, positions);
+
+  for(let i = 0; i < positions.length; i++) {
+    velocities[i] = vector.add(
+      velocities[i], vector.multiplyByNumber(0.5 * timeStep, accelerations[i]));
+  }
 
   return { positions, velocities, accelerations };
 }
