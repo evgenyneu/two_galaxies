6c8d78a800818c2604264ba856d79c1ba81f237b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-25 15:04:34 +1000

    Buttons

--- html/0140_controls/js/buttons.js
+++ html/0140_controls/js/buttons.js
@@ -11,7 +11,34 @@ function didClickReload(currentParams) {
   };
 }
 
+function didClickReverseTime(currentParams) {
+  return (e) => {
+    return false; // Prevent default
+  };
+}
+
+
+/**
+  * Stop propagation of events from elements defined by `selector`
+  * to their parents.
+  *
+  * @param  {string} event  A DOM enven, e.g. "mousedown".
+  * @param  {string} selector A CSS selector for child elements.
+  */
+function stopClickPropagation(event, selector) {
+  var buttons = document.querySelectorAll(selector);
+
+  buttons.forEach((button) => {
+    button.addEventListener(event, (e) => e.stopPropagation());
+  });
+}
+
 export function init(currentParams) {
   var reloadButton = document.querySelector(".TwoGalaxies-reloadButton");
   reloadButton.onclick = didClickReload(currentParams);
+
+  var reverseTimeButton = document.querySelector(".TwoGalaxies-reverseTimeButton");
+  reverseTimeButton.onclick = didClickReverseTime(currentParams);
+
+  stopClickPropagation("mousedown", ".TwoGalaxies-bottomButton");
 }
