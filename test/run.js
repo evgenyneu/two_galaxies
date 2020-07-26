71b2cd369c8a30c05969b8de4872fd1858ca76d6
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-26 17:25:49 +1000

    Tests

--- test/run.js
+++ test/run.js
@@ -4,5 +4,9 @@ import '../js/initial_conditions.test.js';
 import '../js/integrator.test.js';
 import '../html/0140_controls/js/sick_slider.test.js';
 import '../html/0140_controls/js/simulation.test.js';
+import '../html/0140_controls/js/simulation/acceleration.test.js';
+import '../html/0140_controls/js/simulation/vector.test.js';
+import '../html/0140_controls/js/simulation/initial_conditions.test.js';
+import '../html/0140_controls/js/simulation/integrator.test.js';
 
 mocha.run();
