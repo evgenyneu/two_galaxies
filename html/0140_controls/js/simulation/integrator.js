ba71a4b68615ae5eb36611d1eb2abb1cda644be2
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-26 17:22:29 +1000

    Optimize performance

--- html/0140_controls/js/simulation/integrator.js
+++ html/0140_controls/js/simulation/integrator.js
@@ -19,8 +19,6 @@ import getAccelerations from './acceleration.js';
 export default function integrateOneStep(timeStep, masses, positions,
                                          velocities, accelerations) {
 
-  var t0 = performance.now();
-
   for(let i = 0; i < positions.length; i++) {
     velocities[i] = vector.add(
       velocities[i], vector.multiplyByNumber(0.5 * timeStep, accelerations[i]));
@@ -35,10 +33,6 @@ export default function integrateOneStep(timeStep, masses, positions,
     velocities[i] = vector.add(
       velocities[i], vector.multiplyByNumber(0.5 * timeStep, accelerations[i]));
   }
-
-  var t1 = performance.now();
-
-  console.log("Elapsed: " + (t1 - t0) + " ms");
 
   return { positions, velocities, accelerations };
 }
