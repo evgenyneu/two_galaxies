91ceabb396e8a6a51de36190514063b42dab8c44
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-27 16:00:53 +1000

    Make it faster

--- html/0140_controls/js/simulation/integrator.js
+++ html/0140_controls/js/simulation/integrator.js
@@ -4,7 +4,8 @@ import getAccelerations from './acceleration.js';
 
 /**
  * Use one step of Leapfrog method to calculate updated positions,
- * velocities and accelerations of bodies.
+ * velocities and accelerations of bodies. The supplied `positions`,
+ * `velocities` and `accelerations` arrays are updated with new values.
  *
  * @param  {number} timeStep    Length of the time increment.
  * @param  {array} masses       The masses of the two cores, i.e. [1, 1.5]
@@ -12,27 +13,24 @@ import getAccelerations from './acceleration.js';
  *                              first two are galaxy cores, the rest are stars.
  * @param  {array} velocities   Velocity vectors of all bodies.
  * @param  {array} accelerations Acceleration vectors of all bodies.
- * @return {object} An object { positions: [], velocities: [], accelerations }
- *                  containing the updated positions, velocities and
- *                  accelerations of all bodies after `timeStep` time interval.
  */
 export default function integrateOneStep(timeStep, masses, positions,
                                          velocities, accelerations) {
 
-  for(let i = 0; i < positions.length; i++) {
-    velocities[i] = vector.add(
-      velocities[i], vector.multiplyByNumber(0.5 * timeStep, accelerations[i]));
-
-    positions[i] = vector.add(
-      positions[i], vector.multiplyByNumber(timeStep, velocities[i]));
-  }
-
-  accelerations = getAccelerations(masses, positions);
+  let halfTimeStep = 0.5 * timeStep;
 
   for(let i = 0; i < positions.length; i++) {
-    velocities[i] = vector.add(
-      velocities[i], vector.multiplyByNumber(0.5 * timeStep, accelerations[i]));
+    for(let k = 0; i < 3; k++) {
+      velocities[i*3 + k] = velocities[i*3 + k] + halfTimeStep * accelerations[i*3 + k];
+      positions[i*3 + k] = positions[i*3 + k] + timeStep * velocities[i*3 + k];
+    }
   }
 
-  return { positions, velocities, accelerations };
+  getAccelerations(masses, positions, accelerations);
+
+  for(let i = 0; i < positions.length; i++) {
+    for(let k = 0; i < 3; k++) {
+      velocities[i*3 + k] = velocities[i*3 + k] + halfTimeStep * accelerations[i*3 + k];
+    }
+  }
 }
