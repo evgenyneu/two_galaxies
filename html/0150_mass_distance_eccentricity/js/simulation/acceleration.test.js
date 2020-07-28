a5a4223d26a27b440588abe7331c57dde92196c9
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-28 10:44:45 +1000

    tests

--- html/0150_mass_distance_eccentricity/js/simulation/acceleration.test.js
+++ html/0150_mass_distance_eccentricity/js/simulation/acceleration.test.js
@@ -4,10 +4,11 @@ var expect = chai.expect;
 
 it('getAccelerations', () => {
   var masses = [1, 1];
-  var positions = [[1, 0, 0], [-1, 0, 0]];
+  var positions = [1, 0, 0, -1, 0, 0];
+  var accelerations = Array(6).fill(-42);
 
-  var result = getAccelerations(masses, positions);
+  getAccelerations(masses, positions, accelerations);
 
-  expect(result[0]).to.deep.equal([-0.25, 0, 0]);
-  expect(result[1]).to.deep.equal([0.25, 0, 0]);
+  expect(accelerations.slice(0, 3)).to.deep.equal([-0.25, 0, 0]);
+  expect(accelerations.slice(3, 6)).to.deep.equal([0.25, 0, 0]);
 });
--- html/0150_mass_distance_eccentricity/js/simulation/integrator.test.js
+++ html/0150_mass_distance_eccentricity/js/simulation/integrator.test.js
@@ -5,41 +5,41 @@ var expect = chai.expect;
 
 it('integrateOneStep', () => {
   const masses = [1, 1];
-  const positionsIn = [[1, 0, 0], [-1, 0, 0]];
-  const velocitiesIn = [[0, 1, 0], [0, -1, 0]];
-  const accelerationsIn = getAccelerations(masses, positionsIn);
+  const positions = [1, 0, 0, -1, 0, 0];
+  const velocities = [0, 1, 0, 0, -1, 0];
+  var accelerations = Array(6).fill(-42);
+  getAccelerations(masses, positions, accelerations);
   const timeStep = 0.01;
 
-  var { positions, velocities, accelerations } = integrateOneStep(
-    timeStep, masses, positionsIn, velocitiesIn, accelerationsIn);
+  integrateOneStep(timeStep, masses, positions, velocities, accelerations);
 
   // Positions
   // --------
 
-  expect(positions.length).to.equal(2);
+  expect(positions.length).to.equal(6);
 
-  expect(positions[0]).to.deep.closeTo([0.9999875, 0.01, 0], 1e-13);
-  expect(positions[1]).to.deep.closeTo([-0.9999875, -0.01, 0], 1e-13);
+  expect(positions.slice(0, 3)).to.deep.closeTo([0.9999875, 0.01, 0], 1e-13);
+  expect(positions.slice(3, 6)).to.deep.closeTo([-0.9999875, -0.01, 0], 1e-13);
 
   // Velocities
   // --------
 
-  expect(velocities.length).to.equal(2);
+  expect(velocities.length).to.equal(6);
 
-  expect(velocities[0]).to.deep.closeTo(
+  expect(velocities.slice(0, 3)).to.deep.closeTo(
     [-0.24998437646471770e-2, 0.9999875014061211, 0], 1e-13);
 
-  expect(velocities[1]).to.deep.closeTo(
+  expect(velocities.slice(3, 6)).to.deep.closeTo(
     [0.24998437646471770e-2, -0.9999875014061211, 0], 1e-13);
 
   // Accelerations
   // --------
 
-  expect(accelerations.length).to.equal(2);
+  expect(accelerations.length).to.equal(6);
 
-  expect(accelerations[0]).to.deep.closeTo(
+  expect(accelerations.slice(0, 3)).to.deep.closeTo(
     [-0.24996875292943543, -0.0024997187757790514, 0], 1e-13);
 
-  expect(accelerations[1]).to.deep.closeTo(
+  expect(accelerations.slice(3, 6)).to.deep.closeTo(
     [0.24996875292943543, 0.0024997187757790514, 0], 1e-13);
 });
