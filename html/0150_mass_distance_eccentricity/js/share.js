ae05f79eb4b820757fceafeecd2f928e3fd7dd29
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-07-29 15:08:31 +1000

    Share

--- html/0150_mass_distance_eccentricity/js/share.js
+++ html/0150_mass_distance_eccentricity/js/share.js
@@ -1,3 +1,41 @@
 export function getShareURL(initialParams, currentParams) {
-  return 'Hello';
+  let urlParams = getUrlParameters(initialParams, currentParams);
+  let urlStart = getCurrentUrlWithoutParameters();
+  return  `${urlStart}?${urlParams}`;
+}
+
+/**
+ * Returns the last part of the URL containing the parameters
+ */
+export function getUrlParameters(initialParams, currentParams) {
+  initialParams = filterInitialParams(initialParams);
+  return new URLSearchParams(initialParams).toString();
+}
+
+
+/**
+ * Keeps only permitted initial parameters that will be used for sharing.
+ *
+ * @param  {object} initialParams The initial parameters.
+ * @return {object} Initial parameters containing only permitted attributes.
+ */
+export function filterInitialParams(initialParams) {
+  let permittedKeys = [
+    "numberOfRings",
+    "ringSeparation"
+  ];
+
+  let filtered = {};
+
+  permittedKeys.forEach((key) => {
+    if (key in initialParams) {
+      filtered[key] = initialParams[key];
+    }
+  });
+
+  return filtered;
+}
+
+function getCurrentUrlWithoutParameters() {
+  return location.protocol + '//' + location.host + location.pathname;
 }
--- html/0150_mass_distance_eccentricity/js/share.test.js
+++ html/0150_mass_distance_eccentricity/js/share.test.js
@@ -1,11 +1,64 @@
-import { getShareURL } from './share.js';
+import { getShareURL, filterInitialParams, getUrlParameters } from './share.js';
 
 var expect = chai.expect;
 
-describe('getShareURL', () => {
-  it('hi', () => {
-    let result = getShareURL({}, {});
+describe('getUrlParameters', () => {
+  it('returns URL parameters', () => {
+    let initialParams = {
+      numberOfRings: [3, 5],
+      ringSeparation: 2.8
+    };
 
-    expect(result).to.equal("Hello");
+    let currentParams = {};
+
+    let result = getUrlParameters(initialParams, currentParams);
+
+    expect(result).to.equal("numberOfRings=3%2C5&ringSeparation=2.8");
+  });
+
+  it('ingnore unknown parameters', () => {
+    let initialParams = {
+      numberOfRings: [3, 5],
+      ringSeparation: 2.8,
+      unknown: 23
+    };
+
+    let currentParams = {};
+
+    let result = getUrlParameters(initialParams, currentParams);
+
+    expect(result).to.equal("numberOfRings=3%2C5&ringSeparation=2.8");
+  });
+});
+
+describe('getShareURL', () => {
+  it('returns URL for sharing', () => {
+    let initialParams = {
+      numberOfRings: [3, 5],
+      ringSeparation: 2.8
+    };
+
+    let currentParams = {};
+
+    let result = getShareURL(initialParams, currentParams);
+
+    expect(result).to.include("://");
+    expect(result).to.include("?numberOfRings=3%2C5&ringSeparation=2.8");
+  });
+});
+
+describe('filterInitialParams', () => {
+  it('keeps only permitted parameters', () => {
+    let initialParams = {
+      numberOfRings: [3, 5],
+      ringSeparation: 2.8,
+      unknown: 23
+    };
+
+    let result = filterInitialParams(initialParams);
+
+    expect(result).to.have.property('numberOfRings');
+    expect(result).to.have.property('ringSeparation');
+    expect(result).not.to.have.property('unknown');
   });
 });
