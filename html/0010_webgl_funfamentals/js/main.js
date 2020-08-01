6e59c804c65aed9a61e75383d8eea8695831918d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-08-01 13:31:12 +1000

    Clean

--- html/0010_webgl_funfamentals/js/main.js
+++
@@ -1,9 +0,0 @@
-import initWebGl from './init_web_gl.js';
-import render from './render.js';
-
-function main() {
-  var initData = initWebGl();
-  render(initData);
-}
-
-window.onload = main;
--- html/0070_3d_perspective/js/main.js
+++
@@ -1,9 +0,0 @@
-import initWebGl from './init_web_gl.js';
-import drawScene from './render.js';
-
-function main() {
-  var initData = initWebGl();
-  drawScene(initData);
-}
-
-window.onload = main;
